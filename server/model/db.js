d974d829ab6b42720700a80e48eeaae7e61ac70a
victor-shagor <ojoabiolavictor@gmail.com>
2019-06-14 16:19:26 +0100

    feature(endpoint):add user signin

--- server/model/db.js
+++ server/model/db.js
@@ -1,5 +1,28 @@
-const Users = [
+import Helper from "../helpers/helper";
 
+const Users = [
+{
+ id: 1,
+ firstName: 'Ojo',
+ lastName: 'Abiola',
+ email: 'ojoabiola@gmail.com',
+ password: Helper.hashPassword('oladimeji1'),
+ type: 'staff',
+ isAdmin: true,
+ created: new Date(),
+ modified: new Date(),
+},
+{
+ id: 2,
+ firstName: 'Doyin',
+ lastName: 'Adedokun',
+ email: 'doyin@gmail.com',
+ password: Helper.hashPassword('adedoyin1'),
+ type: 'client',
+ isAdmin: false,
+ created: new Date(),
+ modified: new Date(),
+},
 ];
 
 

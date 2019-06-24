d33fb8b8a4288c4fb7f025d6ce61dc814d4bff89
victor-shagor <ojoabiolavictor@gmail.com>
2019-06-24 17:47:09 +0100

    feature(frontend):add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
+
 import userRouter from './server/routes/userrouter';
 
 
 const app = express();
-
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', userRouter);
